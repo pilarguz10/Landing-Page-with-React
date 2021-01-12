3e95f7c41f0c920c1f369681d7f0fa477bdde4cf
pilarguz10 <pilar1494@hotmail.com>
2021-01-12 16:07:59 +0000

    Realizo cambios finales

--- src/js/component/CardInfo.js
+++ src/js/component/CardInfo.js
@@ -1,21 +1,54 @@
 import React from "react";
-import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
-import { CardImg } from "./CardImg.jsx";
-import { Button } from "./Button.jsx";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-export function CardInfo() {
+export function CardInfo(props) {
 	return (
-		<Card>
-			<CardBody>
-				<CardImg />
-				<CardTitle>Card Title</CardTitle>
-				<CardText>
-					Some quick example text to build on the card title and make
-					up the bulk of the cards content.
-				</CardText>
-
-				<Button />
-			</CardBody>
-		</Card>
+		<div className="col-lg-3 col-md-6 mb-4">
+			<div className="card">
+				<img
+					className="card-img-top"
+					src={props.imgURL}
+					alt="Card image cap"
+				/>
+				<div className="card-body">
+					<h5 className="card-title">{props.title}</h5>
+					<p className="card-text">{props.description}</p>
+					<a href={props.buttonURL} className="btn btn-primary">
+						{props.buttonLabel}
+					</a>
+				</div>
+			</div>
+		</div>
 	);
 }
+
+CardInfo.propTypes = {
+	title: PropTypes.string,
+	description: PropTypes.string,
+	buttonLabel: PropTypes.string,
+	buttonURL: PropTypes.string,
+	imgURL: PropTypes.string
+};
+
+// import React from "react";
+// import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+// import { CardImg } from "./CardImg.jsx";
+// import { Button } from "./Button.jsx";
+
+// export function CardInfo() {
+// 	return (
+// 		<Card>
+// 			<CardBody>
+// 				<CardImg />
+// 				<CardTitle>Card Title</CardTitle>
+// 				<CardText>
+// 					Some quick example text to build on the card title and make
+// 					up the bulk of the cards content.
+// 				</CardText>
+
+// 				<Button />
+// 			</CardBody>
+// 		</Card>
+// 	);
+// }
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -27,19 +27,57 @@ export function Home() {
 				boton={"Call to action"}
 			/>
 			<div className="row">
-				<div className="col-sm-3">
-					<CardInfo />
-				</div>
-				<div className="col-sm-3">
-					<CardInfo />
-				</div>
-				<div className="col-sm-3">
-					<CardInfo />
-				</div>
-				<div className="col-sm-3">
-					<CardInfo />
-				</div>
+				<CardInfo
+					title="Aristóteles "
+					description="Fue discípulo de Platón y de otros pensadores, como Eudoxo de Cnido, durante los veinte años que estuvo 
+                    en la Academia de Atenas.5​ Poco después de la muerte de Platón, Aristóteles abandonó Atenas para ser el maestro de Alejandro Magno en el Reino de Macedonia durante casi 5 años.5​ En la última etapa de su vida fundó el Liceo en Atenas, donde enseñó hasta un año antes de su muerte.5​​"
+					buttonLabel="Find out more!"
+					buttonURL="https://es.wikipedia.org/wiki/Arist%C3%B3teles"
+					imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Aristotle_Altemps_Inv8575.jpg/800px-Aristotle_Altemps_Inv8575.jpg"
+				/>
+
+				<CardInfo
+					title="Platón "
+					description="Participó activamente en la enseñanza de la Academia y escribió sus obras, mayoritariamente en forma de diálogo, sobre los más diversos temas, tales como: filosofía política, ética, psicología, antropología filosófica, epistemología, gnoseología, metafísica, cosmogonía, cosmología, filosofía del lenguaje y filosofía de la educación.
+                     A diferencia de sus contemporáneos, se cree que todo su trabajo ha sobrevivido intacto."
+					buttonLabel="Find out more!"
+					buttonURL="https://es.wikipedia.org/wiki/Plat%C3%B3n"
+					imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Plato_Pio-Clemetino_Inv305.jpg/220px-Plato_Pio-Clemetino_Inv305.jpg"
+				/>
+
+				<CardInfo
+					title="Sócrates "
+					description="No hay ninguna evidencia de que Sócrates haya publicado algún escrito de su autoría. Detalles de su vida son conocidos gracias a tres fuentes contemporáneas: los diálogos de Platón, las obras de Aristófanes y los diálogos de Jenofonte. 
+                    En los diálogos de Platón se encuentran los relatos más completos de Sócrates que han sobrevivido desde la antigüedad. "
+					buttonLabel="Find out more!"
+					buttonURL="https://es.wikipedia.org/wiki/S%C3%B3crates"
+					imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Socrate_du_Louvre.jpg/220px-Socrate_du_Louvre.jpg"
+				/>
+
+				<CardInfo
+					title="Heráclito "
+					description="La principal fuente de su vida nos llega a través de Diogenes Laercio. Nació en el seno de una familia aristócrata, pero evitó su vida privilegiada convirtiéndose en un filósofo ermitaño autodidacta. La obra de Heráclito es completamente aforística y se le atribuye un libro titulado Sobre la naturaleza (περὶ φύσεως). 
+                    Como de los demás filósofos griegos anteriores a Platón, no quedan más que fragmentos de sus obras, y en gran parte se conocen sus aportes gracias a testimonios posteriores."
+					buttonLabel="Find out more!"
+					buttonURL="https://es.wikipedia.org/wiki/Her%C3%A1clito"
+					imgURL="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Heraclitus_Rijksmuseum_SK-A-2784.jpeg/800px-Heraclitus_Rijksmuseum_SK-A-2784.jpeg"
+				/>
 			</div>
+
+			{/* <div className="row">
+				<div className="col-sm-3">
+					<CardInfo />
+				</div>
+				<div className="col-sm-3">
+					<CardInfo />
+				</div>
+				<div className="col-sm-3">
+					<CardInfo />
+				</div>
+				<div className="col-sm-3">
+					<CardInfo />
+				</div>
+			</div> */}
 			<Footer />
 		</div>
 	);
